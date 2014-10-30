25a5961760311e66fadbcabee70b2de42191d8be
Allison Larson <allisonlarson91@gmail.com>
2014-10-29 18:52:55 -0600

    rails and ember are on speaking terms

--- ember/app/router.js
+++ ember/app/router.js
@@ -7,6 +7,13 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('landing', { path: '/' });
+  this.route('soundcloud');
+  this.route('google');
+  this.route('lastfm');
+  this.route('twitter');
+  this.resource('users', function() {
+    this.route('show', {path: ':user_id'});
+  });
 });
 
 export default Router;
--- rails/app/controllers/api/sessions_controller.rb
+++ rails/app/controllers/api/sessions_controller.rb
@@ -1,28 +1,30 @@
-class SessionsController < ApplicationController
-
+class Api::SessionsController < ApplicationController
+  respond_to :json
+  
   def create
     auth = request.env['omniauth.auth']
     @identity = Identity.find_from_hash(auth)
     if @identity.nil?
       @identity = Identity.create_from_hash(auth, current_user)
-      respond_with @identity
+      respond_with @identity.user
     end
 
     if signed_in?
       if @identity.user == current_user
-        #redirect to root url
+        respond_with @identity.user
       else
         @identity.user = current_user
         @identity.save
-        #redirect to root
+        respond_with @identity.user
       end
     else
       if @identity.user.present?
         self.current_user = @identity.user
-        #redirect to root
+        respond_with @identity.user
       else
         #redirect to new user
       end
     end
   end
+
 end
