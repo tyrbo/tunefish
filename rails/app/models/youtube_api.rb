2301b51e57eb03b7195db1ff390b0ec1a5ade24d
Hilary Denton <Hilarydenton90@gmail.com>
2014-10-30 14:27:53 -0600

    add fields param so that only have the info we need

--- rails/app/models/youtube_api.rb
+++ rails/app/models/youtube_api.rb
@@ -2,7 +2,7 @@ class YoutubeAPI
   Api_key = ENV['google_key']
 
   def self.get_subscriptions(channel_id=nil)
-    search_url = "/youtube/v3/subscriptions?part=snippet&key=#{Api_key}"
+    search_url = "/youtube/v3/subscriptions?part=snippet&fields=items/snippet/channelId&key=#{Api_key}"
 
     if channel_id
       search_url += "&channelId=#{channel_id}"
@@ -15,12 +15,14 @@ class YoutubeAPI
 
   def self.get_subscription_details(channel_ids)
     ids_string = channel_ids.join(",")
-    connection.get "/youtube/v3/channels?part=contentDetails&fields=items/contentDetails&key=#{Api_key}&id=#{ids_string}"
+    connection.get "/youtube/v3/channels?part=contentDetails&fields=items/contentDetails/relatedPlaylists/uploads&key=#{Api_key}&id=#{ids_string}"
   end
 
   def self.get_uploads(uploads_id)
-    connection.get "/youtube/v3/playlistItems?part=snippet&maxResults=5&key=#{Api_key}&playlistId=#{uploads_id}"
+    connection.get "/youtube/v3/playlistItems?part=snippet&fields=items/snippet/resourceId/videoId&maxResults=5&key=#{Api_key}&playlistId=#{uploads_id}"
   end
+
+  #link to video = https://www.youtube.com/watch?v=#{video_id}
 
   private
 
