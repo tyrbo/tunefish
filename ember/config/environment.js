6f252ac0d6d27fd962ba418fef29e381ed56b02d
Allison Larson <allisonlarson91@gmail.com>
2014-11-01 14:26:00 -0600

    feed page structure styling

--- ember/config/environment.js
+++ ember/config/environment.js
@@ -25,7 +25,8 @@ module.exports = function(environment) {
       'connect-src': "'self' 127.0.0.1:3000 ws://127.0.0.1:35729",
       'img-src': "'self' s3-us-west-2.amazonaws.com",
       'style-src': "'self' maxcdn.bootstrapcdn.com",
-      'frame-src': "'none'"
+      'frame-src': "youtube.com w.soundcloud.com",
+      'report-uri': 'http://localhost:4200'
     },
   };
 
