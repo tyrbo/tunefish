69feead5789402db2dfd47cf1b5d323074bfff67
Hilary Denton <Hilarydenton90@gmail.com>
2014-11-03 17:06:56 -0700

    changed it back

--- rails/app/models/youtube_api.rb
+++ rails/app/models/youtube_api.rb
@@ -1,11 +1,11 @@
 class YoutubeAPI
   Api_key = ENV['google_key']
 
-  def self.get_subscriptions(username=nil)
+  def self.get_subscriptions(channel_id=nil)
     search_url = "/youtube/v3/subscriptions?part=snippet&fields=items/snippet&key=#{Api_key}"
 
-    if username
-      search_url += "&forUsername=#{username}"
+    if channel_id
+      search_url += "&channelId=#{channel_id}"
     else
       search_url += '&mine=true'
     end
