2e07ea9ab27c2b255bc34a95ae43d3089e77429d
Will Faurot <wfaurot@gmail.com>
2014-11-03 20:22:03 -0700

    Urls are dynamically plugged in to activities template

---
+++ ember/app/controllers/activity.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export default Ember.ObjectController.extend({
+  isYoutube: function() {
+    return this.get('provider') === 'youtube';
+  }.property('provider'),
+
+  isSoundcloud: function() {
+    return this.get('provider') === 'soundcloud'
+  }.property('provider')
+});
