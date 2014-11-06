030295c6f2fd5d621b9737d82b01c1f55a88f81d
Will Faurot <wfaurot@gmail.com>
2014-11-06 14:33:39 -0700

    Better naming in youtube_subscription_spec.rb

--- rails/spec/models/youtube_subscription_spec.rb
+++ rails/spec/models/youtube_subscription_spec.rb
@@ -2,18 +2,18 @@ require 'rails_helper'
 
 RSpec.describe YoutubeSubscription, :type => :model do
   before do
-    @ys = YoutubeSubscription.new(title: "Example title",
-                                  channel_id: "12345",
-                                  user_id: 1)
+    @youtube_subscription = YoutubeSubscription.new(title: "Example title",
+                                                    channel_id: "12345",
+                                                    user_id: 1)
   end
 
   it 'has the correct attributes' do
-    expect(@ys).to respond_to :title
-    expect(@ys).to respond_to :channel_id
-    expect(@ys).to respond_to :tracked
+    expect(@youtube_subscription).to respond_to :title
+    expect(@youtube_subscription).to respond_to :channel_id
+    expect(@youtube_subscription).to respond_to :tracked
   end
 
   it 'should default tracked to false' do
-    expect(@ys.tracked). to eq false
+    expect(@youtube_subscription.tracked). to eq false
   end
 end
