5c13cbcb7380fc3b3914ee9face6e172115f28e6
Allison Larson <allisonlarson91@gmail.com>
2014-11-06 12:17:28 -0700

    ember is talking to rails for subscriptions

--- ember/app/controllers/subscriptions.js
+++ ember/app/controllers/subscriptions.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
+  itemController: 'subscription',
   actions: {
     close: function() {
       return this.send('closeModal');
--- ember/app/routes/activities/index.js
+++ ember/app/routes/activities/index.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-    return this.store.find('activity');
-  },
+    return { activities: this.store.find('activity'),
+            youtube_subscriptions: this.store.find('youtube_subscription')
+          };
+    },
 });
