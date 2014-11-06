a39479898091f2c42951c7950a067160fa2d015d
Jonmichael Chambers <jon@tyrbo.net>
2014-11-06 14:48:18 -0700

    Fixed omniauth and various prod/dev env settings

--- ember/app/routes/application.js
+++ ember/app/routes/application.js
@@ -3,17 +3,6 @@ var ApplicationRoute;
 
 ApplicationRoute = Ember.Route.extend({
  actions: {
-    openAuthWindow: function(service) {
-      var env = JSON.parse(decodeURIComponent($('meta[name="tunefish/config/environment"]').attr('content')));
-
-      if (env.environment === 'production') {
-        window.open('http://api.tunefi.sh/auth/' + service);
-      } else {
-        window.open('http://127.0.0.1:3000/auth/' + service);
-      }
-
-      this.transitionTo('activities');
-    },
     openModal: function(name, content) {
       this.controllerFor(name).set('content', content);
       this.render(name, {
