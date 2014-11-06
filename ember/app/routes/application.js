36b3ac5cc965e7980a4c4251a94ba8c40f56a68d
Jonmichael Chambers <jon@tyrbo.net>
2014-11-06 13:52:42 -0700

    Fixed the open auth window thing that I broke.

--- ember/app/routes/application.js
+++ ember/app/routes/application.js
@@ -5,11 +5,14 @@ ApplicationRoute = Ember.Route.extend({
  actions: {
     openAuthWindow: function(service) {
       var env = JSON.parse(decodeURIComponent($('meta[name="tunefish/config/environment"]').attr('content')));
+
       if (env.environment === 'production') {
         window.open('http://api.tunefi.sh/auth/' + service);
       } else {
         window.open('http://127.0.0.1:3000/auth/' + service);
       }
+
+      this.transitionTo('activities');
     },
     openModal: function(name, content) {
       this.controllerFor(name).set('content', content);
--- ember/app/routes/modal.js
+++ ember/app/routes/modal.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-    return 'hi mom'
+    return 'hi mom';
   }
 
 });
