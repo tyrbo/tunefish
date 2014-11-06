a502764e8446fd0f65ba8deda30512b514a2ece9
Will Faurot <wfaurot@gmail.com>
2014-11-06 11:42:43 -0700

    Add specs for YoutubeSubscription

--- rails/spec/models/youtube_subscription_spec.rb
+++ rails/spec/models/youtube_subscription_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe YoutubeSubscription, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @ys = YoutubeSubscription.new(title: "Example title",
+                                  channel_id: "12345",
+                                  user_id: 1)
+  end
+
+  it 'has the correct attributes' do
+    expect(@ys).to respond_to :title
+    expect(@ys).to respond_to :channel_id
+    expect(@ys).to respond_to :tracked
+  end
+
+  it 'should default tracked to false' do
+    expect(@ys.tracked). to eq false
+  end
 end
