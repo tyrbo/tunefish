2fc91873df1e9b9faeaf5c1f2b716e90153f0122
Will Faurot <wfaurot@gmail.com>
2014-11-06 09:37:37 -0700

    Update SessionsController

--- rails/app/controllers/sessions_controller.rb
+++ rails/app/controllers/sessions_controller.rb
@@ -36,10 +36,11 @@ class SessionsController < ApplicationController
         render :close_window
       end
     end
-    if auth['provider'] == 'google_oauth2'
-      params[:subscriptions_hash] = @identity.user.subscriptions(YoutubeAPI.get_subscriptions(current_user))
-      @identity.user.add_tracked_subscriptions(params[:subscriptions_hash])
-    end
+    # subscriptions now have their own table
+    # if auth['provider'] == 'google_oauth2'
+    #   params[:subscriptions_hash] = @identity.user.subscriptions(YoutubeAPI.get_subscriptions(current_user))
+    #   @identity.user.add_tracked_subscriptions(params[:subscriptions_hash])
+    # end
   end
 
   private
