fa6ffb703b48750433b4ee9ff8d906eec9cc042e
Hilary Denton <Hilarydenton90@gmail.com>
2014-11-08 17:41:52 -0700

    ruby logic for percentages done

--- rails/app/models/activity.rb
+++ rails/app/models/activity.rb
@@ -2,7 +2,24 @@ class Activity < ActiveRecord::Base
   belongs_to :user
 
   scope :youtube,    -> { where(type: "YoutubeActivity") }
-  scope :lastfm,     -> { where(type: "LastfmActivity") }
   scope :soundcloud, -> { where(type: "SoundcloudActivity") }
   scope :twitter,    -> { where(type: "TwitterActivity") }
+
+  def self.youtube_percentage
+    decimal_percentage = youtube.count.to_f/all.count.to_f
+    float_percentage = decimal_percentage * 100
+    float_percentage.to_i
+  end
+
+  def self.soundcloud_percentage
+    decimal_percentage = soundcloud.count.to_f/all.count.to_f
+    float_percentage = decimal_percentage * 100
+    float_percentage.to_i
+  end
+
+  def self.twitter_percentage
+    decimal_percentage = twitter.count.to_f/all.count.to_f
+    float_percentage = decimal_percentage * 100
+    float_percentage.to_i
+  end
 end
