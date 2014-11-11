50da2324400c13c865e469738d98eae2f2d89583
Hilary Denton <Hilarydenton90@gmail.com>
2014-11-10 18:42:39 -0700

    I have PERCENTAGES

--- rails/app/serializers/activity_serializer.rb
+++ rails/app/serializers/activity_serializer.rb
@@ -1,28 +1,6 @@
 class ActivitySerializer < ActiveModel::Serializer
   embed :ids, include: true
 
-  attributes :id, :provider, :url, :percentage
+  attributes :id, :provider, :url
   has_one :user
-
-  def youtube_percentage
-    decimal_percentage = Activity.youtube.count.to_f/Activity.all.count.to_f
-    float_percentage = decimal_percentage * 100
-    float_percentage.to_i
-  end
-
-  def soundcloud_percentage
-    decimal_percentage = Activity.soundcloud.count.to_f/Activity.all.count.to_f
-    float_percentage = decimal_percentage * 100
-    float_percentage.to_i
-  end
-
-  def twitter_percentage
-    decimal_percentage = Activity.twitter.count.to_f/Activity.all.count.to_f
-    float_percentage = decimal_percentage * 100
-    float_percentage.to_i
-  end
-
-  def percentage
-    send("#{provider}_percentage")
-  end
 end
