0166fb5ddc64d5d20d1300e253380bf5c6be013a
Hilary Denton <Hilarydenton90@gmail.com>
2014-11-11 13:50:34 -0700

    everything up and working with master, but i vroke the filters in my merge

--- ember/app/routes/activities/index.js
+++ ember/app/routes/activities/index.js
@@ -3,6 +3,6 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
     return { activity: this.store.find('activity'),
-            subscription: this.store.find('youtube_subscription')}
+            subscription: this.store.find('youtube_subscription')};
     },
 });
