f0a71a015a4fbd4d3c5a96cea742b378c9e96e8c
Allison Larson <allisonlarson91@gmail.com>
2014-11-11 10:06:50 -0700

    Add seeds for users and activities

--- rails/db/seeds.rb
+++ rails/db/seeds.rb
@@ -1,7 +1,36 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+user1 = User.create(name: "Jon Snow")
+user2 = User.create(name: "Ned Stark")
+user3 = User.create(name: "Will Faurot")
+user4 = User.create(name: "Allison Larson")
+
+YoutubeActivity.create(provider: "youtube", url: "//www.youtube.com/embed/VO3__R3LWJ0", user_id: user1.id)
+SoundcloudActivity.create(provider: "soundcloud", url: "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/163306920&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true", user_id: user1.id)
+TwitterActivity.create(provider: "twitter", user_id: user1.id, url: "\u003Cblockquote
+                                                  class=\"twitter-tweet\"\u003E\u003Cp\u003E\u003Ca href=\"https:\/\/twitter.com\/hashtag\/tf?src=hash\"\u003E#tf\u003C\/a\u003E\u003C\/p\u003E&mdash;
+                                                  Will Faurot (@Will_Faurot) \u003Ca href=\"https:\/\/twitter.com\/Will_Faurot\/status\/529292521097416705\"\u003ENovember
+                                                  3, 2014\u003C\/a\u003E\u003C\/blockquote\u003E\n\u003Cscript async src=\"\/\/platform.twitter.com\/widgets.js\"
+                                                  charset=\"utf-8\"\u003E\u003C\/script\u003Eki")
+
+YoutubeActivity.create(provider: "youtube", url: "//www.youtube.com/embed/VO3__R3LWJ0", user_id: user2.id)
+SoundcloudActivity.create(provider: "soundcloud", url: "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/163306920&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true", user_id: user2.id)
+TwitterActivity.create(provider: "twitter", user_id: user2.id, url: "\u003Cblockquote
+                                                  class=\"twitter-tweet\"\u003E\u003Cp\u003E\u003Ca href=\"https:\/\/twitter.com\/hashtag\/tf?src=hash\"\u003E#tf\u003C\/a\u003E\u003C\/p\u003E&mdash;
+                                                  Will Faurot (@Will_Faurot) \u003Ca href=\"https:\/\/twitter.com\/Will_Faurot\/status\/529292521097416705\"\u003ENovember
+                                                  3, 2014\u003C\/a\u003E\u003C\/blockquote\u003E\n\u003Cscript async src=\"\/\/platform.twitter.com\/widgets.js\"
+                                                  charset=\"utf-8\"\u003E\u003C\/script\u003Eki")
+
+YoutubeActivity.create(provider: "youtube", url: "//www.youtube.com/embed/VO3__R3LWJ0", user_id: user3.id)
+SoundcloudActivity.create(provider: "soundcloud", url: "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/163306920&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true", user_id: user3.id)
+TwitterActivity.create(provider: "twitter", user_id: user3.id, url: "\u003Cblockquote
+                                                  class=\"twitter-tweet\"\u003E\u003Cp\u003E\u003Ca href=\"https:\/\/twitter.com\/hashtag\/tf?src=hash\"\u003E#tf\u003C\/a\u003E\u003C\/p\u003E&mdash;
+                                                  Will Faurot (@Will_Faurot) \u003Ca href=\"https:\/\/twitter.com\/Will_Faurot\/status\/529292521097416705\"\u003ENovember
+                                                  3, 2014\u003C\/a\u003E\u003C\/blockquote\u003E\n\u003Cscript async src=\"\/\/platform.twitter.com\/widgets.js\"
+                                                  charset=\"utf-8\"\u003E\u003C\/script\u003Eki")
+
+YoutubeActivity.create(provider: "youtube", url: "//www.youtube.com/embed/VO3__R3LWJ0", user_id: user4.id)
+SoundcloudActivity.create(provider: "soundcloud", url: "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/163306920&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true", user_id: user4.id)
+TwitterActivity.create(provider: "twitter", user_id: user4.id, url: "\u003Cblockquote
+                                                  class=\"twitter-tweet\"\u003E\u003Cp\u003E\u003Ca href=\"https:\/\/twitter.com\/hashtag\/tf?src=hash\"\u003E#tf\u003C\/a\u003E\u003C\/p\u003E&mdash;
+                                                  Will Faurot (@Will_Faurot) \u003Ca href=\"https:\/\/twitter.com\/Will_Faurot\/status\/529292521097416705\"\u003ENovember
+                                                  3, 2014\u003C\/a\u003E\u003C\/blockquote\u003E\n\u003Cscript async src=\"\/\/platform.twitter.com\/widgets.js\"
+                                                  charset=\"utf-8\"\u003E\u003C\/script\u003Eki")
