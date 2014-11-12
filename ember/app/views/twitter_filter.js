9d744ca1daa676ab73c903de270cd6561a386d2f
Will Faurot <wfaurot@gmail.com>
2014-11-12 11:35:12 -0700

    Change how the activity filters behave
    We used to have checkboxes, now it's a p element that has colored text
    toggled on/off based on what is filtered.

--- ember/app/views/twitter_filter.js
+++ ember/app/views/twitter_filter.js
@@ -3,5 +3,6 @@ import Ember from 'ember';
 export default Ember.View.extend({
   click: function twitterFilter(){
     $(".tweet").toggle();
+    $(".twitter-filter").toggleClass('filter-off');
   }
 });
